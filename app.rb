35e7f8c76a716aedcdf86337caa0fd65f3426d66
Kateryna Strough <katerynastrough@hotmail.com>
2019-08-17 13:04:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,7 +4,18 @@ class App < Sinatra::Base
 
   configure do
     enable :sessions
-    set :session_secret, "secret"
+    set :session_secret, "flatiron"
   end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do 
+    session[:item] = params[:item]
+    @session = session
+    erb :checkout
+  end
+
 
 end
